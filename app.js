fad421d076d3e47d76b51e478c62150b3d789825
Govind <govindsvyadav@gmail.com>
2023-05-19 18:14:14 +0530

    keyboard input, calculate

---
+++ app.js
@@ -0,0 +1,40 @@
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector("input");
+
+const btnInput = (input) => {
+   if(input === "ac"){
+    result.value = "";
+   }else if(input === "del"){
+    result.value = result.value.slice(0,-1);
+   }else if(input === "="){
+      calculate(result.value) ;
+   }else{
+    result.value += input ;
+   }
+}
+
+
+//keyboard input handler
+const keyBoardInputHandler = (e) => {
+
+const keys = ["1","2","3","4","5","6","7","8","9","0","/","*","-","+"];
+
+e.preventDefault() ;
+    if(keys.includes(e.key)){
+        result.value += e.key;
+    }
+    if(e.key === "Enter"){
+       calculate(result.value);
+    }
+    if(e.key === "Backspace"){
+         result.value = result.value.slice(0,-1);
+    }
+    
+}
+    
+document.addEventListener("keydown",keyBoardInputHandler);
+
+const calculate = (valueString) => {
+    const evaluatedResult = eval(valueString);
+    result.value = evaluatedResult ;
+}
